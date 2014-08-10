5ce722c724f1e85a7324aae7b03b0c9f126192ec
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-08-10 22:02:29 +0200

    Try and fix SITE_URL

--- multistatus/settings.py
+++ multistatus/settings.py
@@ -100,3 +100,5 @@ APPEND_SLASH=False
 
 GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
 GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
+
+SITE_URL = "http://github-multi-status.herokuapp.com"
