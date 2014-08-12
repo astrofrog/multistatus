64164a085f49294438ff35205ca6c79dcc634940
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-08-12 20:23:53 +0200

    Remove non-production code

--- multistatus/settings.py
+++ multistatus/settings.py
@@ -16,11 +16,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hl^9*(0=^o$^fxe+-z(@)psz56s=(myxpx685na7wttgz4!ipt'
+SECRET_KEY = os.environ("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
